0dc6ab4f278059ad374abddd0aede7c1d6b4fa15
Matouš Soldát <matoussoldat@email.cz>
2024-04-15 15:21:47 +0200

    Fix n iterations, load data with utils.jl functions

--- code/de/mlde.jl
+++ code/de/mlde.jl
@@ -19,28 +19,25 @@ GC.gc() # For re-runs, GPU allocs by Python sometimes do not get freed automatic
 
 # ___ Data specific parameters ___
 # GB1
-#= data_path = joinpath(@__DIR__, "..", "..", "data", "GB1")
+data_path = joinpath(@__DIR__, "..", "..", "data", "GB1")
 xlxs_file = "elife-16965-supp1.xlsx"
 wt_string = "MQYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE"  # ['V', 'D', 'G', 'V']
 mutation_positions = [39, 40, 41, 54]
-missing_fitness_value = 0.0 =#
+missing_fitness_value = 0.0
 
 # PhoQ
-data_path = joinpath(@__DIR__, "..", "..", "data", "PhoQ")
+#= data_path = joinpath(@__DIR__, "..", "..", "data", "PhoQ")
 xlsx_file = "PhoQ.xlsx"
 wt_string = "MKKLLRLFFPLSLRVRFLLATAAVVLVLSLAYGMVALIGYSVSFDKTTFRLLRGESNLFYTLAKWENNKLHVELPENIDKQSPTMTLIYDENGQLLWAQRDVPWLMKMIQPDWLKSNGFHEIEADVNDTSLLLSGDHSIQQQLQEVREDDDDAEMTHSVAVNVYPATSRMPKLTIVVVDTIPVELKSSYMVWSWFIYVLSANLLLVIPLLWVAAWWSLRPIEALAKEVRELEEHNRELLNPATTRELTSLVRNLNRLLKSERERYDKYRTTLTDLTHSLKTPLAVLQSTLRSLRSEKMSVSDAEPVMLEQISRISQQIGYYLHRASMRGGTLLSRELHPVAPLLDNLTSALNKVYQRKGVNISLDISPEISFVGEQNDFVEVMGNVLDNACKYCLEFVEISARQTDEHLYIVVEDDGPGIPLSKREVIFDRGQRVDTLRPGQGVGLAVAREITEQYEGKIVAGESMLGGARMEVIFGRQHSAPKDE"
 mutation_positions = [284, 285, 288, 289]
-missing_fitness_value = 0.0
+missing_fitness_value = 0.0 =#
 
 alphabet = DESilico.alphabet.protein
 wt_sequence = collect(wt_string)
 sequences = _get_sequences(data_path, "esm-1b_variants.csv", wt_string, mutation_positions)
 sequences_complete = _get_sequences(data_path, "esm-1b_variants_complete.csv", wt_string, mutation_positions)
 fitness = _get_fitness(data_path, "esm-1b_fitness_norm.csv")
-
-seq_embedding_csv_file = "esm-1b_embedding_complete.csv"
-sequence_embeddings = CSV.read(joinpath(data_path, seq_embedding_csv_file), DataFrame)
-sequence_embeddings = [collect(values(row)) for row in eachrow(sequence_embeddings)]
+sequence_embeddings = _get_sequence_emebeddings(data_path, "esm-1b_embedding_complete.csv")
 
 # ___ Mutagenesis ___
 mutagenesis = NoMutagenesis()
@@ -62,7 +59,7 @@ de!(
     screening,
     selection_strategy,
     mutagenesis,
-    n_iterations=348,
+    n_iterations=384,
 )
 println(sequence_space.top_variant)
 length(sequence_space.variants)
