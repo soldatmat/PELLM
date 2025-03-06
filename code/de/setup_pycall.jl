d2961b06ed9524be4b87855785e2e562763acd74
soldatmat <matoussoldat@email.cz>
2025-03-06 14:14:40 +0100

    Add PyCall setup for linux and macos

--- code/de/setup_pycall.jl
+++ code/de/setup_pycall.jl
@@ -2,6 +2,19 @@ using Pkg
 
 Pkg.activate(".")
 
-CONDA_PATH = "C:\\Users\\matou\\anaconda3" # Set path to Conda root folder here.
-ENV["PYTHON"] = joinpath(CONDA_PATH, "envs", "pellm", "python.exe")
+##### Set the following variables according to your system. #####
+OS = "macos" # Set to "windows", "linux" or "macos" depending on your OS.
+CONDA_PATH = "/opt/miniconda3/" # Set path to Conda root folder here.
+
+
+if OS == "windows"
+    ENV["PYTHON"] = joinpath(CONDA_PATH, "envs", "pellm", "python.exe")
+elseif OS == "linux" || OS == "macos"
+    ENV["PYTHON"] = joinpath(CONDA_PATH, "envs", "pellm", "bin", "python")
+end
+
 Pkg.build("PyCall")
+
+using PyCall
+@show PyCall.python
+print("PyCall setup complete. You need to restart Julia for PyCall to start using the specified python isntallation.")
