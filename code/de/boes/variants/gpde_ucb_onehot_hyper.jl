a4f8b9a3e9346ed0632168d0518ef6db2243210d
soldatmat <matoussoldat@email.cz>
2025-05-02 14:50:23 +0200

    Add BOES ablation variants

---
+++ code/de/boes/variants/gpde_ucb_onehot_hyper.jl
@@ -0,0 +1,161 @@
+using Pkg
+
+Pkg.activate(joinpath(@__DIR__, ".."))
+
+using CSV
+using DataFrames
+using FileIO
+using DESilico
+using BOSS
+using OptimizationPRIMA
+
+include("../utils.jl")
+include("lib/embedding_gp.jl")
+include("lib/de_acq_maximizer.jl")
+include("lib/ucb_acquisition_function.jl")
+
+# ___ Arguments ___
+# First argument can specify the index of the sampled starting variant to be used instead of the wild-type variant.
+sampled_starting_variant_idx = nothing
+if length(ARGS) >= 1
+    sampled_starting_variant_idx = parse(Int, ARGS[1])
+end
+
+# ___ Data specific parameters ___
+dataset_name = ARGS[2]
+
+if dataset_name == "GB1"
+# GB1
+    dataset_name = "GB1"
+    data_path = joinpath(@__DIR__, "..", "..", "PELLM", "data", "GB1")
+    wt_string = "MQYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE"  # ['V', 'D', 'G', 'V']
+    mutation_positions = [39, 40, 41, 54]
+    missing_fitness_value = 0.0
+elseif dataset_name == "PhoQ"
+# PhoQ
+    dataset_name = "PhoQ"
+    data_path = joinpath(@__DIR__, "..", "..", "PELLM", "data", "PhoQ")
+    wt_string = "MKKLLRLFFPLSLRVRFLLATAAVVLVLSLAYGMVALIGYSVSFDKTTFRLLRGESNLFYTLAKWENNKLHVELPENIDKQSPTMTLIYDENGQLLWAQRDVPWLMKMIQPDWLKSNGFHEIEADVNDTSLLLSGDHSIQQQLQEVREDDDDAEMTHSVAVNVYPATSRMPKLTIVVVDTIPVELKSSYMVWSWFIYVLSANLLLVIPLLWVAAWWSLRPIEALAKEVRELEEHNRELLNPATTRELTSLVRNLNRLLKSERERYDKYRTTLTDLTHSLKTPLAVLQSTLRSLRSEKMSVSDAEPVMLEQISRISQQIGYYLHRASMRGGTLLSRELHPVAPLLDNLTSALNKVYQRKGVNISLDISPEISFVGEQNDFVEVMGNVLDNACKYCLEFVEISARQTDEHLYIVVEDDGPGIPLSKREVIFDRGQRVDTLRPGQGVGLAVAREITEQYEGKIVAGESMLGGARMEVIFGRQHSAPKDE"
+    mutation_positions = [284, 285, 288, 289]
+    missing_fitness_value = 0.0
+elseif dataset_name == "TrpB"
+# TrpB
+    dataset_name = "TrpB"
+    data_path = joinpath(@__DIR__, "..", "..", "PELLM", "data", "TrpB")
+    wt_string = "MKGYFGPYGGQYVPEILMGALEELEAAYEGIMKDESFWKEFNDLLRDYAGRPTPLYFARRLSEKYGARVYLKREDLLHTGAHKINNAIGQVLLAKLMGKTRIIAETGAGQHGVATATAAALFGMECVIYMGEEDTIRQKLNVERMKLLGAKVVPVKSGSRTLKDAIDEALRDWITNLQTTYYVFGSVVGPHPYPIIVRNFQKVIGEETKKQIPEKEGRLPDYIVACVSGGSNAAGIFYPFIDSGVKLIGVEAGGEGLETGKHAASLLKGKIGYLHGSKTFVLQDDWGQVQVSHSVSAGLDYSGVGPEHAYWRETGKVLYDAVTDEEALDAFIELSRLEGIIPALESSHALAYLKKINIKGKVVVVNLSGRGDKDLESVLNHPYVRERIRLEHHHHHH"
+    mutation_positions = [183, 184, 227, 228] # ['V', 'F', 'V', 'S']
+    missing_fitness_value = 0.0
+end
+
+# ___ Select starting variant ___
+wt_sequence = collect(wt_string)
+if isnothing(sampled_starting_variant_idx)
+    starting_variant = map(pos -> wt_sequence[pos], mutation_positions)
+else
+    starting_variant = load(joinpath(data_path, "sample_1000.jld2"))["variants"][sampled_starting_variant_idx]
+    map(i -> wt_sequence[mutation_positions[i]] = starting_variant[i], eachindex(starting_variant))
+end
+
+# ___ Load data ___
+alphabet = collect(DESilico.alphabet.protein)
+domain_encoder = Dict(alphabet .=> eachindex(alphabet))
+_encode_domain_float(variant::AbstractVector{Char}) = map(symbol -> Float64.(domain_encoder[symbol]), variant)
+_encode_domain(variant::AbstractVector{Char}) = map(symbol -> domain_encoder[symbol], variant)
+domain_decoder = Dict(eachindex(alphabet) .=> alphabet)
+_decode_domain(coords::AbstractVector{Int}) = map(coord -> domain_decoder[coord], coords)
+
+variants_complete = _get_variants(data_path, "esm-1b_variants_complete.csv")
+variant_coords = map(variant -> _encode_domain(variant), variants_complete)
+sequences_complete = _construct_sequences(variants_complete, wt_string, mutation_positions)
+sequences = _get_sequences(data_path, "esm-1b_variants.csv", wt_string, mutation_positions)
+fitness = _get_fitness(data_path, "esm-1b_fitness_norm.csv")
+
+sequence_embeddings = _get_sequence_emebeddings(data_path, "onehot_encoding_complete.csv")
+
+display("data loaded")
+
+# ___ Init GP ___
+screening = DESilico.DictScreening(Dict(sequences .=> fitness), missing_fitness_value)
+
+# tmp save stuff
+variants_complete = map(variant -> join(variant, ""), variants_complete)
+fitness_complete = [screening(sequence) for sequence in sequences_complete]
+df = DataFrame(variant=variants_complete, fitness=fitness_complete)
+CSV.write(joinpath(data_path, "fitness.csv"), df)
+
+fitness_complete = fitness_complete * 133.59427 # !PhoQ max fitness
+df = DataFrame(variant=variants_complete, fitness=fitness_complete)
+CSV.write(joinpath(data_path, "fitness_denorm.csv"), df)
+# --------
+
+embedding_extractor = Dict(sequences_complete .=> sequence_embeddings)
+_extract_embedding(domain_coords::AbstractVector{Float64}) = _extract_embedding(Vector{Int}(domain_coords))
+function _extract_embedding(domain_coords::AbstractVector{Int})
+    residues = _decode_domain(domain_coords)
+    sequence = _construct_sequence(residues, wt_string, mutation_positions)
+    embedding_extractor[sequence]
+end
+
+domain = Domain(;
+    bounds=([1, 1, 1, 1], [20, 20, 20, 20]),
+    discrete=[true, true, true, true],
+)
+
+embedding_size = length(sequence_embeddings[1])
+model = EmbeddingGP(
+    EmbeddingKernel(Matern12Kernel()), # Matern12Kernel(; metric=Euclidean())
+    #Product([truncated(Normal(0, sqrt(1280) / 3.0); lower=0.0)]), # Multivariate dist with one dimension 
+    Product(fill(truncated(Normal(0, sqrt(1280) / 3.0); lower=0.0), embedding_size)),
+    _extract_embedding,
+    [Dirac(0.0)],
+)
+
+data = BOSS.ExperimentDataPrior(Matrix{Float64}(hcat(_encode_domain_float(starting_variant))), hcat([screening(wt_sequence)]))
+problem = BossProblem(;
+    fitness=LinFitness([1]),
+    f=x -> [screening(_construct_sequence(_decode_domain(x), wt_string, mutation_positions))],
+    domain,
+    model,
+    data,
+)
+
+model_fitter = BOSS.OptimizationMAP(;
+    algorithm=NEWUOA(),
+    multistart=20,
+    parallel=false,
+    rhoend=1e-4,
+)
+
+acq_maximizer = DEAcqMaximizer(variant_coords)
+acquisition = UCB()
+options = BossOptions(; info=true, debug=false)
+
+display("model set")
+display("running GP")
+
+# ___ Run GP ___
+# 200 * 1 = 200 iterations of bo!
+for save_idx in 1:200
+    bo!(problem;
+        model_fitter,
+        acq_maximizer,
+        acquisition,
+        term_cond=IterLimit(1),
+        options,
+    )
+
+    display("saving problem")
+    if isnothing(sampled_starting_variant_idx)
+        save(
+            joinpath(@__DIR__, "..", "benchmarks", "ablation", "ucb_onehot_hyper", dataset_name, "wt.jld2"),
+            "problem", problem,
+        )
+    else
+        save(
+            joinpath(@__DIR__, "..", "benchmarks", "ablation", "ucb_onehot_hyper", dataset_name, "variant_$sampled_starting_variant_idx.jld2"),
+            "problem", problem,
+        )
+    end
+end
+
+display("finished")
